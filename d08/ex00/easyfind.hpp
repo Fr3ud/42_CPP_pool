7f30578b5042886dcfa6ad78c024ab3e5e18f82a
Fr3ud <oo.pavelko@gmail.com>
2018-10-13 11:04:29 +0300

    Adds some fixes

--- d08/ex00/easyfind.hpp
+++ d08/ex00/easyfind.hpp
@@ -1,16 +1,25 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-#include <stdexcept>
-#include <algorithm>
 #include <iostream>
+#include <string>
+#include <exception>
+#include <list>
 
 template < typename T >
 int		easyfind( T & t, int n)
 {
-	typename T::iterator	it = std::find(t.begin(), t.end(), n);
-	if (it != t.end())
-			return *it;
+    std::list<int>::const_iterator			it = t.begin(); 
+	std::list<int>::const_iterator			ite = t.end();
+    while (it != ite)
+    {
+	    if (*it == n)
+        {
+            std::cout << "Value was found." << std::endl;
+			return 0;
+        }
+        it++;
+    }
 	throw std::exception();
 }
 
